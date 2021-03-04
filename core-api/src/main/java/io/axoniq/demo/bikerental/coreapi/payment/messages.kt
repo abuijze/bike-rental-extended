8758810a569a4d4159943efb5a4f7189016a2a84
Allard Buijze <allard.buijze@axoniq.io>
2021-03-04 14:53:06 +0100

    Slightly more logical message ordering

--- core-api/src/main/java/io/axoniq/demo/bikerental/coreapi/payment/messages.kt
+++ core-api/src/main/java/io/axoniq/demo/bikerental/coreapi/payment/messages.kt
@@ -7,7 +7,7 @@ data class PreparePaymentCommand(val amount: Int, @RoutingKey val paymentReferen
 data class ConfirmPaymentCommand(@TargetAggregateIdentifier val paymentId: String)
 data class RejectPaymentCommand(@TargetAggregateIdentifier val paymentId: String)
 
-data class PaymentConfirmedEvent(val paymentId: String, val paymentReference: String)
 data class PaymentPreparedEvent(val paymentId: String, val amount: Int, val paymentReference: String)
+data class PaymentConfirmedEvent(val paymentId: String, val paymentReference: String)
 data class PaymentRejectedEvent(val paymentId: String, val paymentReference: String)
 
